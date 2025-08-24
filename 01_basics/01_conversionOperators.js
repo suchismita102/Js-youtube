6a4da7ae30651838ac0cdbfe1bf5b970898249d7
suchismita102 <suchismitaroy0001@gmail.com>
2025-08-24 21:50:28 +0000

    convert

--- 01_basics/01_conversionOperators.js
+++ 01_basics/01_conversionOperators.js
@@ -3,3 +3,28 @@ console.log(typeof score);
 let valueInNumber = Number(score);
 console.log(typeof(valueInNumber));
 console.log(valueInNumber);
+
+let num=null;
+let Value=Number(num);
+console.log(Value);
+let num1=undefined;
+console.log(num1);
+let num2=true;
+let Value2=Number(num2);
+console.log(Value2);
+
+// "33" -> 33
+//"33abc" -> NaN
+// true->1
+let num3=0;
+let Val3=Boolean(num3)
+console.log(Val3); // false
+
+let num4="Suchi";
+let Val4=Boolean(num4)
+console.log(Val4); // true
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
