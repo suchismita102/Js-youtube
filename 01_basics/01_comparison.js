1f6c5c8989dce1c4b34b5ffd1abe83651150bbfb
suchismita102 <suchismitaroy0001@gmail.com>
2025-08-25 19:53:03 +0000

    comparison

---
+++ 01_basics/01_comparison.js
@@ -0,0 +1,20 @@
+console.log(2>1)
+console.log(2<=1)
+console.log(2>=1)
+console.log(2==1)
+console.log(2!=1)
+
+console.log("2">1) //true
+console.log("02">1) //true
+console.log(null==0) //false
+console.log(null>0)//false
+console.log(null<0) //fase
+console.log(null>=0) //true
+//no predictable answer
+console.log(undefined==0) //false
+console.log(undefined<=0) //false
+console.log(undefined>=0)//false
+console.log(undefined>0)//false
+
+console.log("2"==2) //true
+console.log("2"===2) //false 
